98dbbaae52f1a85d2825cc3443c857f36ed7e3cb
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2022-12-18 09:18:11 +0300

    upd

--- apps/template/client/pages/index.vue
+++ apps/template/client/pages/index.vue
@@ -1,32 +1,27 @@
 <template>
   <v-sheet width="100%" class="fill-height" color="blue">
-    <div>
-      {{ height }}
-      {{ heightComputed }}
-    </div>
-    <!--
-    <v-img src="https://storage.yandexcloud.net/cg-twins-2022/baikal-forum/banner%20(1).jpg" :height="heightComputed">
+    <v-img src="https://i.imgur.com/2wD3VJA.jpg" :height="heightComputed" cover>
     <v-row class="fill-height" no-gutters justify="center" align="center">
       <v-card color="transparent" width="600" flat>
         <v-card-title class="text-h3 text-uppercase mb-6 white--text">
-          baikal shop
+          blah blah
         </v-card-title>
 
         <v-card-subtitle class="mb-1 text-body-1 font-weight-medium white--text">
-          Это интернет-магазин форума «Байкал». Здесь продаются сувениры от партнеров форума. Чтобы их купить, зарабатывай байкоины в течение работы на площадке и на форуме
+          Eveniet culpa consequuntur natus sapiente numquam aut et ab
         </v-card-subtitle>
 
         <v-card-actions>
           <v-row>
             <v-col>
-              <v-btn :large="vuetify.breakpoint.smAndDown" :x-large="vuetify.breakpoint.mdAndUp" depressed to="/login"
-                nuxt class="px-8" dark color="purple" block>
+              <v-btn :large="display.smAndDown" :x-large="display.mdAndUp" depressed to="/login"
+                nuxt class="px-8" dark color="green" block>
                 Войти
               </v-btn>
             </v-col>
             <v-col>
-              <v-btn :large="vuetify.breakpoint.smAndDown" :x-large="vuetify.breakpoint.mdAndUp" depressed
-                to="/signup" nuxt class="px-8" dark text block>
+              <v-btn :large="display.smAndDown" :x-large="display.mdAndUp" depressed
+                to="/signup" nuxt class="px-8" color="orange" dark block>
                 Зарегистрироваться
               </v-btn>
             </v-col>
@@ -35,13 +30,12 @@
       </v-card>
     </v-row>
     </v-img>
-    -->
   </v-sheet>
 </template>
 
 <script lang="ts">
   import {defineComponent, computed, getCurrentInstance} from 'vue';
-  import { useDisplay } from 'vuetify';
+  import { useDisplay, useLayout } from 'vuetify';
 
   export default defineComponent({
     transition: 'scroll-x-transition',
@@ -54,11 +48,13 @@
       }
     },
     setup() {
-      const { footer, height } = useDisplay();
+      const display = useDisplay();
+      const layout = useLayout();
+      console.log(layout.getLayoutItem('header'));
 
       const heightComputed = computed(() => {
-        let result =`calc(100vh - ${footer || 100}px)`;
-        const screenHeight = height.value || 0;
+        let result =`calc(100vh - ${display?.footer?.value || 104}px)`;
+        const screenHeight = display.height.value || 0;
         if (screenHeight < 630) {
           result = '400';
         }
@@ -66,7 +62,7 @@
       });
       return {
         heightComputed,
-        height,
+        display,
       };
     },
   })
