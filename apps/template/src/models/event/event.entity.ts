b637cda0b95f710b438f4d1303bdef7e7b6b9cb8
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2023-04-25 10:07:24 +0300

    upd

---
+++ apps/template/src/models/event/event.entity.ts
@@ -0,0 +1,39 @@
+
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    OneToMany,
+    // ManyToOne,
+    BaseEntity,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { EventUser } from './event-user.entity';
+
+
+@Entity()
+export class Event extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @OneToMany(() => EventUser, (user) => user.event, { eager: false })
+    users: EventUser[];
+
+    @Column({
+        nullable: true,
+    })
+    name: string;
+
+    @Column({
+        type: 'text',
+        nullable: true,
+    })
+    description: string;
+
+    @CreateDateColumn()
+    created: Date;
+
+    @UpdateDateColumn()
+    updated: Date;
+}
--- apps/template/src/models/index.ts
+++ apps/template/src/models/index.ts
@@ -3,12 +3,14 @@ import { Balance, TransactionCodeEnum } from './balance';
 import { Purchase } from './purchase';
 import { Product, ProductTypeEnum } from './product';
 import { Track } from './track';
+import { Event, EventUser } from './event';
 
 export * from './users';
 export * from './balance';
 export * from './product';
 export * from './purchase';
 export * from './track';
+export * from './event';
 
 export const entities = {
   Users,
@@ -16,6 +18,8 @@ export const entities = {
   Purchase,
   Product,
   Track,
+  Event,
+  EventUser,  
 };
 
 export const resolvers = [
