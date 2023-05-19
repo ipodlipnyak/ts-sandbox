07fb289e1ac533f9313d79d0ca27d557a0be4671
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2023-05-19 20:16:38 +0300

    upd

--- libs/google/src/google.service.ts
+++ libs/google/src/google.service.ts
@@ -9,12 +9,14 @@ import {
 import { OAuth2Client, OAuth2ClientOptions, GoogleAuth } from 'google-auth-library';
 import { google, Auth } from 'googleapis';
 
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = [
+    'https://www.googleapis.com/auth/calendar',
+    // 'https://www.googleapis.com/auth/calendar.readonly',
+];
 
 @Injectable({ scope: Scope.REQUEST })
 export class GoogleService {
     private client: OAuth2Client;
-    // private auth: Auth.OAuth2Client;
     private auth: Auth.GoogleAuth;
 
     constructor(@Inject('MY_GOOGLE_OPTIONS') readonly opts?: any) {
@@ -23,29 +25,61 @@ export class GoogleService {
             return;
         }
         this.client = new OAuth2Client(opts);
-        // this.auth = new google.auth.OAuth2(opts);
         this.auth = new google.auth.GoogleAuth({
             keyFile: opts.applicationCredentials,
-            scopes: SCOPES, 
+            scopes: SCOPES,
         })
     }
 
     get ClientInstance() {
         return this.client;
     }
-    
+
     get ClientId() {
         return this.client._clientId;
     }
 
-    async getCalendar(): Promise<any[]> {
-        // const auth = this.auth;
+    /**
+     * Calendar api v3
+     * @see https://developers.google.com/calendar/api/v3/reference
+     */
+    get calendarV3() {
         const auth = this.auth;
         const cal = google.calendar({
             version: 'v3',
             auth,
         });
-        const result = await cal.calendarList.list();
-        return [result];
+        return cal;
+    }
+
+    /**
+     * Accept an invitation and insert calendar to service account calendars list.
+     * This methods helps to subscribe service account to existing calendars from another accounts.
+     * 
+     * @param id calendar id with invitation
+     * 
+     * @see https://developers.google.com/calendar/api/v3/reference/calendarList/insert#python
+     */
+    async addCalendar(id: string) {
+        const response = await this.calendarV3.calendarList.insert({
+            requestBody: {
+                id
+            },
+        });
+        return response.data;
+    }
+
+    /**
+     * @see https://stackoverflow.com/questions/26064095/inserting-google-calendar-entries-with-service-account
+     */
+    async getCalendar(): Promise<any[]> {
+        let response = undefined;
+        try {
+            response = await this.calendarV3.calendarList.list();
+        } catch(error) {
+            console.log(error);
+            //
+        }
+        return [response.data];
     }
 }
