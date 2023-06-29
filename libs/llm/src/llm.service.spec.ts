d23a19e2e37258f5c51852941ab95aff45765d8c
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2023-06-29 15:30:22 +0300

    llm test fix

--- libs/llm/src/llm.service.spec.ts
+++ libs/llm/src/llm.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LLMService } from './llm.service';
+import { HttpService } from '@nestjs/axios';
 
 describe('LLMService', () => {
   let service: LLMService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LLMService],
+      providers: [
+        HttpService,
+        LLMService,
+        {
+          provide: 'MY_LLM_OPTIONS',
+          useValue: [],
+        },
+        {
+          provide: 'AXIOS_INSTANCE_TOKEN',
+          useValue: '',
+        }
+      ],
     }).compile();
 
-    service = module.get<LLMService>(LLMService);
+    service = await module.resolve<LLMService>(LLMService);
   });
 
   it('should be defined', () => {
