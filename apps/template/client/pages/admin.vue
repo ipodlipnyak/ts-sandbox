a7d8234b9f43f9f9a7afd036260b75117533b040
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2024-04-04 12:41:05 +0300

    admin space

---
+++ apps/template/client/pages/admin.vue
@@ -0,0 +1,33 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    definePageMeta({
+      layout: 'my',
+      scrollToTop: true,
+      pageTransition: {
+        // name: 'scroll-x-transition',
+        name: 'fade-transition',
+        mode: 'out-in'
+        // duration: {
+        //   enter: 1000,
+        //   leave: 0,
+        // },
+      },
+      layoutTransition: {
+        name: 'fade-transition',
+      },
+      middleware: [
+        'auth',
+        'admin',
+      ],
+    });
+    return {};
+  }
+})
+</script>
---
+++ apps/template/client/pages/admin/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useDisplay, useLayout } from 'vuetify';
+
+export default defineComponent({
+  middleware(ctx) {
+    // return ctx.redirect('/admin/db');
+  },
+
+  setup() {
+    const display = useDisplay();
+
+    return {
+      display,
+    };
+  },
+})
+</script>
