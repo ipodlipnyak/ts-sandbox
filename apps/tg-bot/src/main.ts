0481c875ff0c71261755194b1dfd59fe96b10e5a
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2025-01-31 16:10:55 +0300

    upd

--- apps/tg-bot/src/main.ts
+++ apps/tg-bot/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { TgBotModule } from './tg-bot.module';
 
+const RABBITMQ_URL = process.env?.RABBITMQ_URL || 'amqp://localhost:5672';
+const QUEUE = process.env?.RABBITMQ_QUEUE_TG || 'tgQueue';
+
 async function bootstrap() {
-  const app = await NestFactory.create(TgBotModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(TgBotModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [ RABBITMQ_URL ],
+      queue: QUEUE,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  await app.listen();
 }
 bootstrap();
