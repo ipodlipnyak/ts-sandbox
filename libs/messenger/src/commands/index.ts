638fe3013412ba0b95cb7e3dc798a6f61a9a4b3d
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2025-02-21 15:15:44 +0300

    bot commands update

--- libs/messenger/src/commands/index.ts
+++ libs/messenger/src/commands/index.ts
@@ -1,5 +1,6 @@
 import { TelegramCommandsListRunner } from './telegram-commands-list.command';
+import { TelegramCommandsUpdateRunner } from './telegram-commands-update.command';
 import { TelegramWebhookInfoRunner } from './telegram-webhook-info.command';
 import { TelegramWebhookSetRunner } from './telegram-webhook-set.command';
 
-export const commands = [TelegramWebhookInfoRunner, TelegramWebhookSetRunner, TelegramCommandsListRunner];
+export const commands = [TelegramWebhookInfoRunner, TelegramWebhookSetRunner, TelegramCommandsListRunner, TelegramCommandsUpdateRunner];
